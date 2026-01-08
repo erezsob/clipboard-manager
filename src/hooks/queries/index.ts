8661562e7a7f4ef155ba11f01213524fb41fb412
Erez <erez.sobel@nordsec.com>
2026-01-08 18:22:22 +0100

    chore: format

--- src/hooks/queries/index.ts
+++ src/hooks/queries/index.ts
@@ -1,8 +1,8 @@
-export { useHistoryQuery, flattenHistoryPages } from "./useHistoryQuery";
+export type { HistoryPage, InfiniteHistoryData } from "./types";
+export { useClipboardMonitor } from "./useClipboardMonitor";
 export {
+	useClearHistoryMutation,
 	useDeleteItemMutation,
 	useToggleFavoriteMutation,
-	useClearHistoryMutation,
 } from "./useHistoryMutations";
-export { useClipboardMonitor } from "./useClipboardMonitor";
-export type { HistoryPage, InfiniteHistoryData } from "./types";
+export { flattenHistoryPages, useHistoryQuery } from "./useHistoryQuery";
